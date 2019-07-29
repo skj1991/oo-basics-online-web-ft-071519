06bb37715121180137bc77346cc692f65f2d9cd8
Stacy Johns <stacykjohns@icloud.com>
2019-07-29 23:49:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,8 +16,8 @@ class Book
     @author
   end
 
-  def page_count=(page_count)
-    @page_count = page_count
+  def page_count=(num)
+    @page_count = num
   end
   def page_count
     @page_count
