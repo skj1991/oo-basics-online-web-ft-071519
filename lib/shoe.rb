21d9934a33b18d046b6362cadff3ff84dad95506
Stacy Johns <stacykjohns@icloud.com>
2019-07-29 23:59:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -23,5 +23,20 @@ class Shoe
   def size
     @size
   end
-  
+
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
 end
